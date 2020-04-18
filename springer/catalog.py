208be6e053925afbb217d9cc53ce43ada1a7064a
JnyJny <erik.oshaughnessy@gmail.com>
2020-04-18 15:52:12 -0500

    Enhanced list functions, file-backed defaults
    To support the CLI [get|set]-default-catalog subcommands the
    Catalog class needed to grow some persistence for language and
    topic settings. Added methods and properties for reading and
    writing language and topic from `catalog_defaults.toml` saved
    to the path obtained from `typer.get_app_dir()`.
    
    Reworked all the list output to make it easier to digest visually
    but remain parsable by other command-line tools. I'm pretty happy
    with it right now, not perfect but running out of give a crap.

--- springer/catalog.py
+++ springer/catalog.py
@@ -1,8 +1,8 @@
 """the Springer Free Textbook Catalog
 """
 
-
 import pandas
+import toml
 import typer
 import requests
 
@@ -57,13 +57,18 @@ class Catalog:
 
     def __init__(self, language: Language = None, topic: Topic = None):
         """
-        :param url: str
+        :param language: springer.constants.Language
+        :param topic: springer.constants.Topic
         """
-        self.language = language or Language.English
-        self.topic = topic or Topic.AllDisciplines
+
+        self.language = language or Language(self.defaults.get("language", "en"))
+        self.topic = topic or Topic(self.defaults.get("topic", "all"))
 
         if not self.cache_file.exists():
-            self.fetch_catalog()
+            try:
+                self.fetch_catalog()
+            except KeyError as error:
+                raise error from None
 
     def __repr__(self):
 
@@ -72,33 +77,35 @@ class Catalog:
         )
 
     def __str__(self):
-        desc = self.topic.name.replace("_", " ")
-        s = []
-        s.append(f"{Token.Catalog.value}         URL: {self.url}")
-        s.append(
-            f"{Token.Catalog.value}    Language: {self.language}/{self.language.name}"
-        )
-        s.append(f"{Token.Catalog} Topic: {self.topic}/{desc!r}")
-        s.append(f"{Token.Catalog}  Cache File: {self.cache_file}")
-        s.append(f"{Token.Catalog}       Books: {self.dataframe.count().max()}")
-        s.append(f"{Token.Catalog}    Packages: {len(self.packages)}")
 
-        return "\n".join(s)
+        return f"{Token.Catalog}|{self.name}"
 
     def __iter__(self):
         return self.books()
 
     @property
-    def name(self):
+    def name(self) -> str:
+        """An identifier formed from `topic-language`."""
         try:
             return self._name
         except AttributeError:
             pass
-        self._name = f"{self.topic}-{self.language}"
+        self._name = f"{self.language}-{self.topic}"
         return self._name
 
     @property
+    def is_default(self) -> bool:
+        """Returns True if this catalog has the default language and topic."""
+
+        return Catalog().name == self.name
+
+    @property
     def url(self) -> str:
+        """The URL hosting the Excel-formated file for this catalog.
+
+        Accessing URL can raise KeyError for language/topic combinations
+        which do not exist.
+        """
         try:
             return self._url
         except AttributeError:
@@ -108,13 +115,14 @@ class Catalog:
 
         return self._url
 
-    def content_url(self, file_format: FileFormat) -> str:
-        """The URL prefix for downloading content.
+    def content_url(self, uid: str, file_format: FileFormat) -> str:
+        """The content download URL for `uid` with `file_format`.
         
+        :param uid: str
         :param file_format: springer.constants.FileFormat
         :return: str
         """
-        return _urls["content"][file_format]
+        return _urls["content"][file_format] + f"/{uid}.{file_format}"
 
     @property
     def app_dir(self) -> Path:
@@ -129,6 +137,37 @@ class Catalog:
         self._app_dir.mkdir(mode=0o755, exist_ok=True)
 
         return self._app_dir
+
+    @property
+    def defaults_file(self) -> Path:
+        """Path to the default Catalog configuration TOML file."""
+        try:
+            return self._defaults_file
+        except AttributeError:
+            pass
+        self._defaults_file = self.app_dir / "catalog_defaults.toml"
+        return self._defaults_file
+
+    @property
+    def defaults(self) -> dict:
+        """A dictionary loaded from the file `self.defaults_file`.
+        """
+        try:
+            contents = toml.decoder.load(self.defaults_file)
+        except FileNotFoundError:
+            contents = {}
+        return contents
+
+    def save_defaults(self) -> None:
+        """
+        """
+        updated = {"language": self.language.value, "topic": self.topic.value}
+        try:
+            contents = toml.decoder.load(self.defaults_file)
+        except FileNotFoundError:
+            contents = {}
+        contents.update(updated)
+        toml.encoder.dump(contents, self.defaults_file.open("w"))
 
     @property
     def cache_file(self) -> Path:
@@ -148,7 +187,7 @@ class Catalog:
         """A pandas.DataFrame populated with the contents of the Springer free textbook catalog.
 
         Column names are lower-cased and embedded spaces replaced with underscores.
-        Columns added: uid, package_name
+        Columns added: uid, package_name, title
         Columns removed: english_package_name, german_package_name, "Unnamed: 0"
 
         The dataframe is sorted by book_title in ascending order.
@@ -177,7 +216,18 @@ class Catalog:
         # casing the resultant string. The column names should then be
         # valid python identifiers which makes the dataframe easier to use.
 
-        df.columns = [c.lower().replace(" ", "_") for c in df.columns]
+        columns = [c.lower().replace(" ", "_") for c in df.columns]
+
+        # Referring to textbook.book_title was getting jarring.
+        # Renamed the 'book_title' column to just 'title'
+
+        book_title = columns.index("book_title")
+        columns.insert(book_title, "title")
+        columns.remove("book_title")
+
+        # Ok that's enough.
+
+        df.columns = columns
 
         # German language catalogs have 'German Package Name' and 'English
         # Package Name' columns where the English column is empty. To make
@@ -195,12 +245,12 @@ class Catalog:
         # - unique
         # - a valid file name for most modern filesystems.
         #
-        # Settled on {book_title}-{springer_section}-{eisbn} and then
+        # Settled on {title}-{springer_section}-{eisbn} and then
         # applying a translationg table, `_ttable` to the resulting string
         # to clean up punctuation and embedded spaces, periods and slashes.
 
-        df["path"] = (
-            df["book_title"]
+        df["filename"] = (
+            df["title"]
             .str.cat(df["uid"], sep="-")
             .apply(lambda v: v.translate(_ttable))
         )
@@ -211,7 +261,7 @@ class Catalog:
 
     @property
     def packages(self) -> dict:
-        """Dictionary of pandas.DataFrames whose keys are eBook package names.
+        """Dictionary of pandas.DataFrames values whose keys are eBook package names.
         """
         try:
             return self._packages
@@ -237,6 +287,21 @@ class Catalog:
         """
         pandas.read_excel(url or self.url).dropna(axis=1).to_csv(self.cache_file)
 
+    def textbooks(self, dataframe: pandas.DataFrame = None) -> tuple:
+        """This generator function returns a namedtuple for each row in `dataframe`.
+
+        If `dataframe` is got supplied, `self.dataframe` is used. 
+
+        :param dataframe: pandas.DataFrame
+        :return: namedtuple called 'TextBook'
+        """
+
+        if dataframe is None:
+            dataframe = self.dataframe
+
+        for textbook in dataframe.itertuples(index=None, name="Textbook"):
+            yield textbook
+
     def download_book(
         self, textbook: tuple, dest: Path, file_format: FileFormat, overwrite: bool,
     ) -> int:
@@ -251,13 +316,13 @@ class Catalog:
 
         dest.mkdir(mode=0o755, exist_ok=True, parents=True)
 
-        path = (dest / textbook.path).with_suffix(file_format.suffix)
+        path = (dest / textbook.filename).with_suffix(file_format.suffix)
 
         if not overwrite and path.exists():
             logger.debug(f"Skipped {path}")
             return 0
 
-        url = f"{self.content_url(file_format)}/{textbook.uid}.{file_format}"
+        url = self.content_url(textbook.uid, file_format)
 
         response = requests.get(url, stream=True)
 
@@ -271,6 +336,49 @@ class Catalog:
                 size += len(chunk)
                 fp.write(chunk)
         return size
+
+    def download_dataframe(
+        self,
+        dest: Path,
+        file_format: FileFormat,
+        overwrite: bool,
+        dataframe: pandas.DataFrame = None,
+    ) -> int:
+        """Downloads all the textbooks in `dataframe` to `dest` with format `file_format`.
+
+        :param dest: pathlib.path
+        :param file_format: springer.constants.FileFormat
+        :param overwrite: bool
+        :param dataframe: pandas.DataFrame
+        :return: int <bytes written>
+        """
+
+        if dataframe is None:
+            dataframe = self.dataframe
+
+        def show_title(item):
+            if not item:
+                return f"Downloaded to {dest}"
+            return item.title[:40]
+
+        total = 0
+        with typer.progressbar(
+            self.textbooks(dataframe),
+            length=len(dataframe),
+            label=f"{self.name}:{file_format:4s}",
+            show_percent=False,
+            show_pos=True,
+            width=10,
+            empty_char=Token.Empty,
+            fill_char=Token.Book,
+            item_show_func=show_title,
+        ) as workitems:
+            for textbook in workitems:
+                total += self.download_book(
+                    textbook, dest, file_format, overwrite=overwrite
+                )
+
+        return total
 
     def download_package(
         self, package: str, dest: Path, file_format: FileFormat, overwrite: bool
@@ -290,59 +398,6 @@ class Catalog:
 
         return self.download_dataframe(dest, file_format, overwrite, df)
 
-    def download_dataframe(
-        self,
-        dest: Path,
-        file_format: FileFormat,
-        overwrite: bool,
-        dataframe: pandas.DataFrame = None,
-    ) -> int:
-        """Downloads all the textbooks in `dataframe` to `dest` with format `file_format`.
-
-        :param dest: pathlib.path
-        :param file_format: springer.constants.FileFormat
-        :param overwrite: bool
-        :param dataframe: pandas.DataFrame
-        :return: int <bytes written>
-        """
-        if dataframe is None:
-            dataframe = self.dataframe
-
-        def show_title(item):
-            if not item:
-                return f"Downloaded to {dest}"
-            return item.book_title[:40]
-
-        with typer.progressbar(
-            self.textbooks(dataframe),
-            length=len(dataframe),
-            label=f"{self.name}:{file_format}",
-            show_percent=False,
-            show_pos=True,
-            width=10,
-            empty_char=Token.Empty,
-            fill_char=Token.Book,
-            item_show_func=show_title,
-        ) as workitems:
-            for textbook in workitems:
-                self.download_book(textbook, dest, file_format, overwrite=overwrite)
-        return
-
-    def textbooks(self, dataframe: pandas.DataFrame = None) -> tuple:
-        """This generator function returns a namedtuple for each row in `dataframe`.
-
-        If `dataframe` is got supplied, `self.dataframe` is used. 
-
-        :param dataframe: pandas.DataFrame
-        :return: namedtuple called 'TextBook'
-        """
-
-        if dataframe is None:
-            dataframe is self.dataframe
-
-        for textbook in dataframe.itertuples(index=None, name="Textbook"):
-            yield textbook
-
     def download(self, dest: Path, file_format: FileFormat, overwrite: bool) -> int:
         """Download all the textbooks in this catalog to `dest` with format `file_format`.
 
@@ -360,51 +415,82 @@ class Catalog:
         :param long_format: bool
         """
 
-        if long_format:
-            print(f"{Token.Book}|NDX|PkgID|Electronic ISBN  |Title|Author")
+        for textbook in self.textbooks(dataframe):
 
-        for index, textbook in enumerate(self.textbooks(dataframe), start=1):
-            if long_format:
-                fields = [
-                    Token.Book,
-                    f"{index:3d}",
-                    f"{textbook.ebook_package}",
-                    f"{textbook.electronic_isbn:17}",
-                    f"{textbook.book_title}",
-                    f"{textbook.author}",
-                ]
-                print("|".join(fields))
-            else:
-                print(f"{Token.Book}|{textbook.book_title}")
+            lines = []
 
-    def list_textbooks(self, long_format: bool) -> None:
-        """Displays all books in a catalog, one line per book.
+            for key, value in textbook._asdict().items():
+                key = key.replace("_", " ").title()
+                lines.append(f"{Token.Book}|{textbook.electronic_isbn}|{key}|{value}")
+            lines.append(f"{Token.Stop}{lines[0][1:]}")
+            if not long_format:
+                lines = lines[:1]
+
+            print("\n".join(lines))
+
+    def list_textbooks(self, long_format: bool, match: str = None) -> None:
+        """Displays all books in a catalog.
+
+        :param long_format: bool
+        :param match: bool
         """
 
-        self.list_dataframe(self.dataframe, long_format)
+        if match:
+            source = self.dataframe[
+                self.dataframe.title.str.contains(match, case=False)
+            ]
+        else:
+            source = self.dataframe
+
+        self.list_dataframe(source, long_format)
 
     def list_package(
         self, name: str, package: pandas.DataFrame, long_format: bool,
     ):
-        """
+        """Displays information about an eBook package with `name`.
+
+        :param name: str
+        :param package: pandas.DataFrame
+        :param long_format: bool
         """
 
-        pid = package.ebook_package.unique()[0]
-        cnt = len(package)
+        ebook = package.ebook_package.unique()[0]
+
+        lines = [f"{Token.Package}|{ebook}|Name|{name}"]
 
         if long_format:
-            print(f"{Token.Package}|#Bk|PkgID|Name")
+            lines.extend(
+                [f"{Token.Package}|{ebook}|Books|{len(package)}",]
+            )
+        print("\n".join(lines))
 
-        print(f"{Token.Package}|{cnt:3d}|{pid}|{name}")
         if long_format:
-            self.list_dataframe(package, True)
+            self.list_dataframe(package, False)
+            print(f"{Token.Stop}|{ebook}|Name|{name}")
 
     def list_packages(self, long_format: bool) -> None:
-        """
-        """
+        """Display package information for all pacakges in the catalog.
 
-        if not long_format:
-            print(f"{Token.Package}|#Bk|PkgID|Name")
+        :param long_format: bool
+        """
 
         for name, package in self.packages.items():
             self.list_package(name, package, long_format)
+
+    def list_catalog(self, long_format: bool) -> None:
+        """Display catalog information.
+
+        :param long_format: bool
+        """
+
+        print(f"{self}|URL|{self.url}")
+        print(f"{self}|Default|{self.is_default}")
+        print(f"{self}|Cache|{self.cache_file}")
+        print(f"{self}|Packages|{len(self.packages)}")
+        print(f"{self}|Books|{len(self.dataframe)}")
+
+        if long_format:
+            for name, package in self.packages.items():
+                self.list_package(name, package, False)
+                self.list_dataframe(package, False)
+        print(f"{Token.Stop}|{self.name}")
